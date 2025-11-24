c99fa34df85ba3e9d8187cb1f7b1cb5b02ed8be7
sviridohahaha <standup.minsk@gmail.com>
2025-11-24 20:57:22 +0300

    KS-1-2

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,16 @@
+package lesson1
+
+fun main() {
+    val ordersAmount: Int = 75
+    val thanksText: String = "Спасибо за покупку"
+
+    println(ordersAmount)
+    println(thanksText)
+
+    var employeesAmount: Int = 2000
+
+   // println(employeesAmount)
+
+    employeesAmount = 2000 - 1
+    println(employeesAmount)
+}
