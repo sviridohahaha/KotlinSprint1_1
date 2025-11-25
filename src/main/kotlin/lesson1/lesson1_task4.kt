7cf0d08674f4f2b7892deac1112333d802bccda4
sviridohahaha <standup.minsk@gmail.com>
2025-11-25 15:30:33 +0300

    KS-1-4

--- src/main/kotlin/lesson1/lesson1_task4.kt
+++ src/main/kotlin/lesson1/lesson1_task4.kt
@@ -5,7 +5,7 @@ fun main() {
     val age: Byte = 27
     val partOfDay: Float = 0.075F
     val seconds: Short = 6480
-    val partOfYear: String = "2.0547945205479453E-4"
+    val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
 
     println(length)
