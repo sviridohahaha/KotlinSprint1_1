a6162bba91130076471ff6be18adc389a05ffd33
sviridohahaha <standup.minsk@gmail.com>
2025-11-26 02:36:54 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson2
+
+fun main() {
+    val studentsAmount = 4F
+    val mark1 = 3
+    val mark2 = 4
+    val mark3 = 3
+    val mark4 = 5
+
+    val averageMark = (mark1 + mark2 + mark3 + mark4) / studentsAmount
+
+    println(averageMark)
+}
