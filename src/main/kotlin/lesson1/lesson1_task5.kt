f901f60e4f730cceda6ae6656d89b96504e1cfb1
sviridohahaha <standup.minsk@gmail.com>
2025-11-25 21:32:23 +0300

    KS-1-5

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,17 +1,16 @@
 package lesson1
 
 fun main() {
-
+    val secondsPerMinute = 60
+    val minutesPerHour = 60
     val seconds = 6480
-    val minutes = seconds / 60
-    val hours = minutes / 60
+    val minutes = seconds / secondsPerMinute
+    val hours = minutes / minutesPerHour
 
     val secondsRemaining = seconds % 60
     val minutesRemaining = minutes % 60
 
-    var hoursForTimer: String = if (hours < 10) "0$hours" else "$hours"
-    var minutesForTimer: String = if (minutesRemaining < 10) "0$minutesRemaining" else "$minutesRemaining"
-    var secondsForTimer: String = if (secondsRemaining < 10) "0$secondsRemaining" else "$secondsRemaining"
+    val time = String.format("%02d:%02d:%02d", hours, minutesRemaining, secondsRemaining)
 
-    println("$hoursForTimer:$minutesForTimer:$secondsForTimer")
+    println(time)
 }
