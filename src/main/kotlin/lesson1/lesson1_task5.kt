95d7a06590aa89bd79fd8729b5de7f137b526c49
sviridohahaha <standup.minsk@gmail.com>
2025-11-26 01:11:17 +0300

    KS-1-5

--- src/main/kotlin/lesson1/lesson1_task5.kt
+++ src/main/kotlin/lesson1/lesson1_task5.kt
@@ -1,16 +1,15 @@
 package lesson1
 
+const val SECONDS_PER_MINUTE = 60
+const val MINUTES_PER_HOUR = 60
+
 fun main() {
-    val secondsPerMinute = 60
-    val minutesPerHour = 60
     val seconds = 6480
-    val minutes = seconds / secondsPerMinute
-    val hours = minutes / minutesPerHour
+    val minutes = seconds / SECONDS_PER_MINUTE
+    val hours = minutes / MINUTES_PER_HOUR
 
-    val secondsRemaining = seconds % 60
-    val minutesRemaining = minutes % 60
+    val secondsRemaining = seconds % SECONDS_PER_MINUTE
+    val minutesRemaining = minutes % MINUTES_PER_HOUR
 
-    val time = String.format("%02d:%02d:%02d", hours, minutesRemaining, secondsRemaining)
-
-    println(time)
+    println(String.format("%02d:%02d:%02d", hours, minutesRemaining, secondsRemaining))
 }
