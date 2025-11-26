633848b1162ecfbe8443e32bb810104616c67c4a
sviridohahaha <standup.minsk@gmail.com>
2025-11-26 21:36:08 +0300

    KS-3-1

---
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+    val firstName = "Татьяна"
+    var lastName = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$lastName $firstName $patronymic, $age")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $patronymic, $age")
+}
