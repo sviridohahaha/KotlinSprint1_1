bb591dba63063526c2a1586fb17fc83e76d5b2e3
sviridohahaha <standup.minsk@gmail.com>
2025-11-27 18:59:53 +0300

    KS-2-4

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,12 +1,14 @@
 package lesson2
 
+const val TOTAL_PERCENT = 100
+
 fun main() {
     var crystalOre = 7
     var ironOre = 11
     val buff = 20
 
-    crystalOre = crystalOre * buff / 100
-    ironOre = ironOre * buff / 100
+    crystalOre = crystalOre * buff / TOTAL_PERCENT
+    ironOre = ironOre * buff / TOTAL_PERCENT
 
     println("Бонусная кристаллическая руда: $crystalOre")
     println("Бонусная железная руда: $ironOre")
