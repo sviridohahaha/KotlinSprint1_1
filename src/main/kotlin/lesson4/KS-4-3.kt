13923b49799f8f40b1ccc9bda88be4b6720a21b8
sviridohahaha <standup.minsk@gmail.com>
2025-11-30 14:12:33 +0300

    KS-4-3

--- src/main/kotlin/lesson4/KS-4-3.kt
+++ src/main/kotlin/lesson4/KS-4-3.kt
@@ -1,12 +1,18 @@
 package lesson4
 
+const val SUNNY_WEATHER = true
+const val TENT_OPEN = true
+const val HUMIDITY_REQUIRED = 20
+const val BAD_SEASON = "зима"
+
 fun main(){
     val isSunny = true
     val isTentOpen = true
     val humidity = 20
     val season = "зима"
 
-    val areConditionsGood = isSunny && isTentOpen && (humidity == 20) && (season != "зима")
+    val areConditionsGood = isSunny == SUNNY_WEATHER && isTentOpen == TENT_OPEN &&
+            humidity == HUMIDITY_REQUIRED && season != BAD_SEASON
 
     println("Благоприятные ли условия сейчас для роста бобовых? $areConditionsGood")
 }
