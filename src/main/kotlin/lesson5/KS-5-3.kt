a0733bca64774018355b52fd1a4cea1873df4637
sviridohahaha <standup.minsk@gmail.com>
2025-11-30 22:57:36 +0300

    KS-5-3

--- src/main/kotlin/lesson5/KS-5-3.kt
+++ src/main/kotlin/lesson5/KS-5-3.kt
@@ -1,8 +1,10 @@
 package lesson5
 
 fun main() {
-    val winNumber1 = 13
-    val winNumber2 = 29
+    val winNumber1 = (0..42).random()
+    val winNumber2 = (0..42).random()
+
+    println("Введите два числа от 0 до 42:")
 
     val number1 = readLine()?.toIntOrNull()
     val number2 = readLine()?.toIntOrNull()
@@ -12,5 +14,7 @@ fun main() {
     } else if ((number1 == winNumber1 || number1 == winNumber2) || (number2 == winNumber1 || number2 == winNumber2)) {
         println("Вы выиграли утешительный приз!")
     } else println("Неудача!")
+
+    println("Выигрышные числа: $winNumber1 и $winNumber2")
 
 }
