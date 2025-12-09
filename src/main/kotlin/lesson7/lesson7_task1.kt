092a73a7d990af136853b86ff28f65c65021149e
Zhenya <standup.minsk@gmail.com>
2025-12-09 22:14:25 +0300

    KS-7-1

--- src/main/kotlin/lesson7/lesson7_task1.kt
+++ src/main/kotlin/lesson7/lesson7_task1.kt
@@ -1,14 +1,13 @@
 package lesson7
 
 fun main() {
-    val part1 = ('a'..'z').random()
-    val part2 = (0..9).random()
-    val part3 = ('a'..'z').random()
-    val part4 = (0..9).random()
-    val part5 = ('a'..'z').random()
-    val part6 = (0..9).random()
+    val passwordLength = 6
+    var password = ""
 
-    val password = "$part1$part2$part3$part4$part5$part6"
-
+    for (i in 1..passwordLength) {
+        val part = if (i % 2 == 1) ('a'..'z').random()
+        else (0..9).random()
+        password += part
+    }
     println(password)
 }
