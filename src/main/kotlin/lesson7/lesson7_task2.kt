fe97230c2a4bc833f30177d3e98e3727da34ca98
Zhenya <standup.minsk@gmail.com>
2025-12-09 23:18:30 +0300

    KS-7-2

--- src/main/kotlin/lesson7/lesson7_task2.kt
+++ src/main/kotlin/lesson7/lesson7_task2.kt
@@ -1,13 +1,16 @@
 package lesson7
 
 fun main() {
-    while (true) {
-        val code = (1000..9999).random()
+    var code: Int
+
+    do {
+        code = (1000..9999).random()
         println("Your authorization code: $code")
         println("Enter your authorization code:")
-        if (readln().toInt() == code) {
-            println("Welcome!")
-            break
-        } else println("You entered the wrong code!")
+        val enteredCode = readln().toInt()
+
+        if (enteredCode == code) println("Welcome!")
+        else println("You entered the wrong code!")
     }
+    while (enteredCode != code)
 }
