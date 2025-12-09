18d5631384cc1f78e90d0c87bcb5873b7224bdcd
Zhenya <standup.minsk@gmail.com>
2025-12-09 12:36:59 +0300

    KS-8-4

---
+++ src/main/kotlin/lesson8/lesson8_task4.kt
@@ -0,0 +1,20 @@
+package lesson8
+
+fun main() {
+    val ingredients = arrayOf("bun", "sausage", "sauce", "fried onion")
+
+    for (i in ingredients) println(i)
+
+    println("Enter the unnecessary ingredient:")
+    val unnecessaryIngredients = readln()
+
+    if (ingredients.contains(unnecessaryIngredients)) {
+        println("Enter a new ingredient:")
+        val newIngredient = readln()
+
+        ingredients[ingredients.indexOf(unnecessaryIngredients)] = newIngredient
+
+        println("Ready! You have saved the recipe:")
+        for (i in ingredients) println(i)
+    } else println("There is no such ingredient in the recipe")
+}
