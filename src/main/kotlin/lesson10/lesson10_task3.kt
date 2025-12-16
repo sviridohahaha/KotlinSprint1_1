abaa204f5f0495c9cf03ed564f419b6b34b9a23b
Zhenya <standup.minsk@gmail.com>
2025-12-17 01:32:54 +0300

    KS-10-3

---
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -0,0 +1,26 @@
+package lesson10
+
+fun main() {
+    println("Enter the password length:")
+    val passwordLength = readln().toInt()
+
+    if (passwordLength < 2) {
+        println("Password length is too short.")
+    } else println("Your password: ${generatePassword(passwordLength)}")
+}
+
+fun generatePassword(length: Int): String {
+    val digits = "0123456789"
+    val specialChars = "!\"#$%&'()*+,-./ "
+
+    val password = mutableListOf<Char>()
+
+    for (i in 0 until length) {
+        if (i % 2 == 0) {
+            password.add(digits.random())
+        } else {
+            password.add(specialChars.random())
+        }
+    }
+    return password.joinToString("")
+}
