97a962f49d42d50d73bb8036be180021c987053e
Zhenya <standup.minsk@gmail.com>
2025-12-23 01:56:28 +0300

    KS-11-2

--- src/main/kotlin/lesson_11/User2.kt
+++ src/main/kotlin/lesson_11/User2.kt
@@ -12,19 +12,14 @@ class User2(
         println("User details: ID - $id, Username - $login, Password - $password, Email - $email, bio - $bio")
     }
 
-    fun updateBio() {
-        println("Enter your bio:")
-        bio = readln()
+    fun updateBio(newBio: String) {
+        bio = newBio
         println("Your bio has been updated")
     }
 
-    fun changePassword() {
-        println("Enter current password:")
-        val currentPassword = readln()
-
+    fun changePassword(currentPassword: String, newPassword: String) {
         if (currentPassword == password) {
-            println("Enter new password")
-            password = readln()
+            password = newPassword
             println("Your password has been changed. Your new password is $password")
         } else println("Current password is incorrect. Your password has not been  changed.")
     }
@@ -39,7 +34,7 @@ fun main() {
         null
     )
 
-    newUser.updateBio()
-    newUser.changePassword()
+    newUser.updateBio("I'm the best football player in the world!")
+    newUser.changePassword("7777777", "I'm the best of the best")
     newUser.userInformation()
 }
