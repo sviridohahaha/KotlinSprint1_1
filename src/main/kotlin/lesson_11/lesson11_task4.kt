cb027583a816090394cab9825f8c3dbdfcfb062a
Zhenya <standup.minsk@gmail.com>
2026-01-05 23:49:49 +0300

    KS-11-4

---
+++ src/main/kotlin/lesson_11/lesson11_task4.kt
@@ -0,0 +1,28 @@
+package lesson_11
+
+class RecipeCategory(
+    val title: String,
+    val description: String,
+    val imageUrl: String,
+) {
+
+}
+
+class Ingredient(
+    val name: String,
+    val quantity: Double,
+    val unit: String,
+) {
+
+}
+
+class Recipe(
+    val title: String,
+    val description: String,
+    val cookingTime: Int,
+    val imageUrl: String,
+    val ingredients: Set<Ingredient>,
+    val instruction: String,
+) {
+
+}
