793201de9f2ef19d7a9a219d21bf4b28d714302f
Zhenya <standup.minsk@gmail.com>
2026-01-19 16:39:42 +0300

    KS-15-5

--- src/main/kotlin/lesson_15/lesson15_task5.kt
+++ src/main/kotlin/lesson_15/lesson15_task5.kt
@@ -10,17 +10,18 @@ interface PassengerTransportable {
 }
 
 interface CargoTransportable {
-    fun loadCargo(cargo: Double)
-    fun unloadCargo(cargo: Double)
+    fun loadCargo(cargo: Int)
+    fun unloadCargo(cargo: Int)
 }
 
 abstract class Transport(val maxPassengers: Int) : Movable, PassengerTransportable {
     var currentPassenger = 0
-    val freePassengerSlots = maxPassengers - currentPassenger
+    var freePassengerSlots = maxPassengers - currentPassenger
 
     override fun loadPassenger(passengers: Int) {
         if (passengers <= freePassengerSlots) {
             currentPassenger += passengers
+            freePassengerSlots -= passengers
             println(
                 "$passengers passenger(s) loaded. Current passenger(s): $currentPassenger." +
                         " You can load ${maxPassengers - currentPassenger} more passengers"
@@ -33,6 +34,7 @@ abstract class Transport(val maxPassengers: Int) : Movable, PassengerTransportab
     override fun unloadPassenger(passengers: Int) {
         if (passengers <= currentPassenger) {
             currentPassenger -= passengers
+            freePassengerSlots += passengers
             println("$passengers passenger(s) unloaded. Current passenger(s): $currentPassenger")
         } else {
             println("Not enough passengers to unload")
@@ -45,22 +47,22 @@ class Car : Transport(maxPassengers = 3) {
 }
 
 class Truck : Transport(maxPassengers = 1), CargoTransportable {
-    val maxCargo = 2.0
-    var currentCargo = 0.0
+    val maxCargo = 2
+    var currentCargo = 0
 
     override fun move() = println("Truck is moving")
 
-    override fun loadCargo(cargo: Double) {
+    override fun loadCargo(cargo: Int) {
         if (currentCargo + cargo <= maxCargo) {
             currentCargo += cargo
             println(
                 "Cargo loaded: $cargo. Current cargo: $currentCargo." +
-                        "  You can load ${maxCargo - currentCargo} more tons of cargo into this truck"
+                        " You can load ${maxCargo - currentCargo} more tons of cargo into this truck"
             )
         } else println("Cargo exceeds max limit")
     }
 
-    override fun unloadCargo(cargo: Double) {
+    override fun unloadCargo(cargo: Int) {
         if (cargo <= currentCargo) {
             currentCargo -= cargo
             println("Cargo unloaded: $cargo. Current cargo: $currentCargo")
@@ -70,33 +72,31 @@ class Truck : Transport(maxPassengers = 1), CargoTransportable {
 
 fun main() {
     val truck = Truck()
-    val car = Car()
+    val car1 = Car()
+    val car2 = Car()
 
-    var cargoForTransportation = 2.0
+    var cargoForTransportation = 2
     var passengersForTransportation = 6
 
-    do {
-        truck.loadCargo(
-            if (truck.maxCargo >= cargoForTransportation) cargoForTransportation
-            else truck.maxCargo
-        )
-        cargoForTransportation -= truck.currentCargo
-        truck.loadPassenger(
-            if (truck.maxPassengers >= passengersForTransportation) passengersForTransportation
-            else truck.maxPassengers
-        )
-        passengersForTransportation -= truck.currentPassenger
-        car.loadPassenger(
-            if (car.maxPassengers >= passengersForTransportation) passengersForTransportation
-            else car.maxPassengers
-        )
-        passengersForTransportation -= car.currentPassenger
-        truck.move()
-        car.move()
-        truck.unloadCargo(truck.currentCargo)
-        truck.unloadPassenger(truck.currentPassenger)
-        car.unloadPassenger(car.currentPassenger)
-        truck.move()
-        car.move()
-    } while (cargoForTransportation != 0.0 || passengersForTransportation != 0)
+    truck.loadCargo(truck.maxCargo)
+    cargoForTransportation -= truck.currentCargo
+    truck.loadPassenger(truck.maxPassengers)
+    passengersForTransportation -= truck.currentPassenger
+
+    car1.loadPassenger(car1.maxPassengers)
+    passengersForTransportation -= car1.currentPassenger
+
+    car2.loadPassenger(passengersForTransportation)
+    passengersForTransportation -= car2.currentPassenger
+
+    truck.move()
+    car1.move()
+    car2.move()
+
+    truck.unloadCargo(truck.currentCargo)
+    truck.unloadPassenger(truck.currentPassenger)
+    car1.unloadPassenger(car1.currentPassenger)
+    car2.unloadPassenger(car2.currentPassenger)
+
+    println("It remains to transport $passengersForTransportation people and $cargoForTransportation tons of cargo")
 }
