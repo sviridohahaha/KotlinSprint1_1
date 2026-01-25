0148989acc3859c353509da1bff6af0226081e8a
Zhenya <standup.minsk@gmail.com>
2026-01-26 01:16:34 +0300

    KS-19-4

---
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -0,0 +1,36 @@
+package lesson_19
+
+import java.util.Locale.getDefault
+
+enum class Ammo(val damage: Int) {
+    EMPTY(0),
+    BLUE(5),
+    GREEN(10),
+    RED(20),
+}
+
+class Tank {
+    private var currentAmmo = Ammo.EMPTY
+
+    fun loadAmmo(ammo: Ammo) {
+        currentAmmo = ammo
+        println("Tank armed with ${ammo.name.lowercase(getDefault())} ammo")
+    }
+
+    fun shoot() {
+        if (currentAmmo == Ammo.EMPTY) println("Tank is not armed with any ammo!")
+        else println("Tank caused damage: ${currentAmmo.damage}")
+    }
+}
+
+fun main() {
+    val tank = Tank()
+
+    tank.shoot()
+    tank.loadAmmo(Ammo.BLUE)
+    tank.shoot()
+    tank.loadAmmo(Ammo.GREEN)
+    tank.shoot()
+    tank.loadAmmo(Ammo.RED)
+    tank.shoot()
+}
