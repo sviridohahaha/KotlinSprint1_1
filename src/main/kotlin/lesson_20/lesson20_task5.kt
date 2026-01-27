e005f30f01d8a08e86945f5c64bdb166e3ea75ec
Zhenya <standup.minsk@gmail.com>
2026-01-27 18:52:16 +0300

    KS-20-5

--- src/main/kotlin/lesson_20/lesson20_task5.kt
+++ src/main/kotlin/lesson_20/lesson20_task5.kt
@@ -8,15 +8,16 @@ class Robot {
         "I am a robot, and I love playing FIFA",
         "It is a nice day to play a few games!",
         "I prefer to play for \"Atletico\" Madrid",
-        "I will win you!"
+        "I will win you!",
     )
 
     var currentPhrase = phrases[Random.nextInt(phrases.size)]
+    var phraseModifier: (String) -> String = { it }
 
-    fun say() = println(currentPhrase)
+    fun say() = println(phraseModifier(currentPhrase))
 
     fun setModifier(modifier: (String) -> String) {
-        currentPhrase = modifier(currentPhrase)
+        this.phraseModifier = modifier
     }
 }
 
