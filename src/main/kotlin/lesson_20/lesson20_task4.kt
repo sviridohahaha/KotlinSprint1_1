fd9ce19fd04494a49dc25f4fc9a5fee2cec23f0c
Zhenya <standup.minsk@gmail.com>
2026-01-27 07:09:28 +0300

    KS-20-4

--- src/main/kotlin/lesson_20/lesson20_task4.kt
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -3,10 +3,7 @@ package lesson_20
 fun main() {
     val elements = listOf("Item1", "Item2", "Item3", "Item4", "Item5", "Item6")
 
-    val actions = elements.mapIndexed { index, element ->
-        if (index % 2 == 1)
-            println("Нажат элемент $element")
-    }
+    val actions = elements.map { element -> "Нажат элемент $element" }
 
-    actions.forEach { it }
+    for (i in 0..actions.size) if (i % 2 == 1) println(actions[i])
 }
